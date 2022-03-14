38be7cbe9ece03713fbe8f5e424986968db7ae72
Nestor Ojeda <nojeda@adasoft.es>
2022-03-14 12:36:36 +0100

    Nuevo campo relacionado

---
+++ models/project_model.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields 
+
+
+class CustomProject(models.Model):
+     _inherit = "project.project"
+
+     ref_cliente = fields.Char(
+          comodel_name='sale.order',  
+          related='sale_order_id.client_order_ref', 
+          string='Referencia del Cliente',
+          readonly=True, 
+          store=True
+          )
